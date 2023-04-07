d31dca4ece2b6e4f2f30dc65fa6b5ef5eb100b66
Daniel Mendes de Almeida <daniel.m.almeida.97@gmail.com>
2023-04-07 19:25:41 -0300

    toggleTheme
    configurando o button toggleTheme para fazer mudanças automáticas, e configurando o tamanho dos icon dos buttons de linguagens

--- src/app/Shared/header/header.component.ts
+++ src/app/Shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -17,15 +17,44 @@ export class HeaderComponent implements OnInit {
   projetos: string = ""
   @Input()
   contato: string = ""
+
   constructor() { }
 
   ngOnInit(): void {
+    setInterval(() => {
+      this.autoToggleTheme();
+    }, 60 * 60 * 1000);
   }
 
+  
   toggleTheme() {
     const body = document.body;
     const isDark = body.classList.toggle('dark-theme');
     this.themeIcon = isDark ? 'brightness_4' : 'brightness_2';
+    console.log(this.themeIcon)
+  }
+
+  @Output()
+  autoToggleThemeEvent = new EventEmitter<void>();
+  
+  autoToggleTheme(){
+    const hour = new Date();
+    const currentHour = hour.getHours();
+    const body = document.body;
+
+    if (currentHour >= 18 || currentHour < 6) {
+      // horário noturno
+      body.classList.add('dark-theme');
+      this.themeIcon = 'brightness_4';
+    } else {
+      // horário diurno
+      body.classList.remove('dark-theme');
+      this.themeIcon = 'brightness_2';
+    }
+
+    this.autoToggleThemeEvent.emit();
+
+    console.log("Agora é:", currentHour)
   }
 
 }
