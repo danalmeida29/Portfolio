39cb416b1313f17a311c036ef6ad9c8f081bfb63
Daniel Mendes de Almeida <daniel.m.almeida.97@gmail.com>
2023-04-13 18:26:29 -0300

    textDescription
    Mudança e correções no texto de apresentação.

--- src/app/Components/about/about.component.ts
+++ src/app/Components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -7,11 +7,12 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  @Input()
-  textDescription: string = ""
   constructor() { }
 
   ngOnInit(): void {
+
   }
+  
+
 
 }
